230dd80949ee638a780d56149bffbc1c4f8fa042
mayahawkins <maya.t.hawkins@gmail.com>
2014-09-14 21:35:57 -0400

    Creating BST
    Start to create a BST

--- Main.java
+++ Main.java
@@ -1,4 +1,17 @@
-class Main {
-    public static void main ( String [] args ) {
-    }
+public class BST{
+
+
+
+
+public static void main ( String [] args ) {
+	System.out.println("Hey!")
+}
+
+
+class BST_Node{
+
+	int here;
+	BST left;
+	BST right;
+
 }
