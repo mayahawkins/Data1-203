23453c662855c73c651baa9bab17bfcd7979f1e9
mayahawkins <maya.t.hawkins@gmail.com>
2014-09-18 14:25:09 -0400

    Remove
    Created remove function

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ interface BST {
 	public BST empty();
 	public boolean isEmpyHuh(BST);
 	public BST add(int x);
+	public BST remove(int x);
 
 }
 
@@ -13,22 +14,27 @@ class BST_MT implements BST{
 	public int cardinality(){
 		return 0;add
 	}
+
 	public boolean member (int x){
 		return false;
 	}
+
 	public BST empty(){
 		return new BST_MT();
 	}
+
 	public boolean isEmpyHuh(BST_MT){
 		return false;
 	}
 
 
 	public BST add(int x){
-	return new BST(BST_MT(), int x, BST_MT());
+	return new BST(new BST_MT(), int x, new BST_MT());
 	}
 
-
+	public BST remove (int x) {
+		return new BST_MT();
+	}
 
 }
 
@@ -48,6 +54,7 @@ class BST_Node implements BST {
 	public int cardinality( /* BST_Node this */ ){
 		return this.left.cardinality() + this.right.cardinality() + 1;
 	}
+
 	public boolean member (int x) {
 		if (x == this.here) {
 			return true; 
@@ -57,6 +64,7 @@ class BST_Node implements BST {
 		}
 		else {return this.right.member(x);}
 	}
+
 	public BST empty(){
 		return new BST_MT();
 	}
@@ -64,6 +72,7 @@ class BST_Node implements BST {
 	public boolean isEmpyHuh(BST_Node){
 		return true;
 	}
+
 	public BST add(int x){
 		if (x == this.here) {
 			return new BST(this.left, this.here, this.right);
@@ -71,13 +80,25 @@ class BST_Node implements BST {
 		else if (x < this.here) {
 		return new BST_Node(this.left.add(x), this.here, this.right)
 		}
-		else {return new BST_Node(this.left, this.here this.right.add(x))}
+		else {return new BST_Node(this.left, this.here this.right.add(x))
+		}
+	}
+
+	public BST remove(int x){
+		if (x == this.here) {
+			return new BST_Node(this.left, this.right, this.right.right);
+		}
+		else if (x < this.here) {
+			return new BST_Node(this.left.remove(x), this.here, this.right);
+		}
+		else {return new BST_Node(this.left, this,here, this.right.remove(x));
+		}
 	}
 }
 
 class Main {
-public static void main ( String [] args ) {
-	System.out.println("Hey!");
+	public static void main ( String [] args ) {
+		System.out.println("Hey!");
 
-}
+	}
 }
